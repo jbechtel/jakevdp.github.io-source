03b2d3124f47c08acdc2732e062de524504f9294
jbechtel <jbechtel91@gmail.com>
2018-04-04 18:20:41 -0700

    Edits to ADPs

--- push-to-github.sh
+++ push-to-github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pelican content -o output -s publishconf.py
 ghp-import output
-git push git@github.com:jbechtel/jbechtel.github.io.git gh-pages:master -f
+git push git@github.com:jbechtel/jbechtel.github.io.git gh-pages:master
